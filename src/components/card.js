225daae7cb28a68f9e759bb5e7b67329c568d722
Lucamilne <lucamilne@googlemail.com>
2025-11-15 15:40:44 +0000

    Added stats

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useMemo } from 'react';
 import Image from 'next/image'
 import PokemonBallSprite from '@/assets/icons/tiers/Bag_Poké_Ball_Sprite.png'
 import ElementalTypes from './elemental-types.js';
-import { rarityIcons } from '@/utils/rarityIcons.js';
+import Stats from './stats.js';
 
 export default function Card({ pokemonCard, isPlayerCard, index }) {
     const [isFlipped, setIsFlipped] = useState(false);
@@ -24,18 +24,18 @@ export default function Card({ pokemonCard, isPlayerCard, index }) {
     return (
         <div className="relative select-none card">
             <div className={`p-3 border-front rounded-md aspect-square ${isFlipped ? 'card-shown' : 'card-hidden'}`}>
-                <div className={`${bgGradient} relative w-full aspect-square border border-1 border-black`}>
+                <div className={`${bgGradient} relative w-full aspect-square border border-1 border-black overflow-hidden`}>
                     <div className="relative h-full flex flex-col items-center justify-center shadow-inner">
+                        <Stats stats={pokemonCard.stats} originalStats={pokemonCard.originalStats} />
                         <ElementalTypes types={pokemonCard.types} />
                         <Image width={96} height={96} className="mt-2 z-10" alt={pokemonCard.name} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonCard.id}.png`} />
                         <div className='absolute bottom-0'>
                             <svg className="w-full rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><path d="M0 0v4c250 0 250 96 500 96S750 4 1000 4V0H0Z" fill={isPlayerCard ? "#7dbdff" : "#ff6d64"}></path></svg>
                             <div className={`pt-6 px-2 py-1.5 text-center text-white text-xs font-bold truncate text-black bg-black w-full uppercase text-shadow-md ${isPlayerCard ? "bg-theme-blue-accent" : "bg-theme-red-accent"}`}>
-                                <span className={`${pokemonCard.rarity} px-2 py-0.5 rounded-full shadow-md`}>{pokemonCard.name}</span>
+                                <span className={`${pokemonCard.rarity} px-2 py-0.5 rounded-full shadow-md tracking-widest`}>{pokemonCard.name}</span>
                             </div>
                         </div>
                     </div>
-                    {/* <Stats className="absolute top-0 left-0 mt-0.5 z-20" stats={pokemonCard.stats} originalStats={pokemonCard.originalStats} /> */}
                 </div>
             </div>
             <div className={`border-back absolute top-0 left-0 w-full rounded-md p-3 select-none aspect-square shadow ${!isFlipped ? 'card-shown' : 'card-hidden'}`}>
---
+++ src/components/stats.js
@@ -0,0 +1,35 @@
+import { useMemo } from 'react';
+
+export default function Stats({ stats, originalStats }) {
+    const conditionalClass = (index) => {
+        const stat = stats[index];
+        const originalStat = originalStats[index];
+
+        if (stat > 9) {
+            return 'text-yellow-500';
+        } else if (stat > originalStat) {
+            return 'text-green-500';
+        } else if (stat < originalStat) {
+            return 'text-red-500';
+        } else {
+            return 'text-white';
+        }
+    };
+
+    const formattedStats = useMemo(() => {
+        return stats.map(value => value > 9 ? 'A' : value);
+    }, [stats]);
+
+    return (
+        <div className="absolute top-0 left-0 bg-linear-to-b from-black/15 to-transparent px-1 pb-1 w-full flex">
+            <div className="mt-0.5 font-mono text-sm font-bold leading-none text-shadow-lg flex flex-col justify-between text-center">
+                <div className={conditionalClass(1)}>{formattedStats[1]}</div>
+                <div className="flex justify-between gap-4">
+                    <div className={`${conditionalClass(0)}`}>{formattedStats[0]}</div>
+                    <div className={`${conditionalClass(2)}`}>{formattedStats[2]}</div>
+                </div>
+                <div className={conditionalClass(3)}>{formattedStats[3]}</div>
+            </div>
+        </div>
+    );
+}
