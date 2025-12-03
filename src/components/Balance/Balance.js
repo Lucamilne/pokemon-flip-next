8fb85bed7db429bfd5668df3e9a2a53a91f0c6f6
Luca Milne <lucamilne@googlemail.com>
2025-12-03 19:04:28 +0000

    Stronger cards buffed

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -3,7 +3,7 @@ export default function Balance({ score }) {
         <div className="absolute top-8 bottom-8 right-8 border-4 border-black w-12">
             <div className="grid grid-rows-10 h-full w-full">
                 {Array.from({ length: 10 }, (_, i) => (
-                    <div key={i} className={i < (10 - score) ? "balance-red" : ` balance-blue`}></div>
+                    <div key={i} className={i < (10 - score) ? "balance-red" : `balance-blue ${i === 10 - score ? 'border-t-4 border-white' : ''}`}></div>
                 ))}
             </div>
         </div>
