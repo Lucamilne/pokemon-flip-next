1a6b4ec21109e8587d71aa9862d2ff150dc710cc
Lucamilne <lucamilne@googlemail.com>
2025-12-10 12:38:36 +0000

    Card image and text sizing tweak

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -22,7 +22,8 @@ export default function Stats({ stats, originalStats }) {
 
     return (
         <div className="absolute top-0 left-0 bg-linear-to-b from-black/15 to-transparent px-1 pb-1 w-full flex">
-            <div className="mt-0.5 font-mono text-xs z-20 font-bold leading-none text-shadow-md/50 flex flex-col justify-between text-center">                <div className={conditionalClass(1)}>{formattedStats[1]}</div>
+            <div className="mt-0.5 font-mono text-sm z-20 font-bold leading-none text-shadow-md/50 flex flex-col justify-between text-center">
+                <div className={conditionalClass(1)}>{formattedStats[1]}</div>
                 <div className="flex justify-between gap-5">
                     <div className={`${conditionalClass(0)}`}>{formattedStats[0]}</div>
                     <div className={`${conditionalClass(2)}`}>{formattedStats[2]}</div>
